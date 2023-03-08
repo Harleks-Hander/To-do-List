895ccf49ee75ce001da89ed91da28ae4e95ca26e
alex <adekolaoluwaa@gmail.com>
2023-03-08 13:53:38 +0100

    phone

--- Toodles.js
+++ Toodles.js
@@ -10,6 +10,12 @@ const ClearBtn = document.querySelector(".clear")
 
 const Check = document.querySelector(".ckeck")
 
+let Inside = InputEl.value
+let internal = [`<ul class="listt"><li class="listtt">${Inside}</li> <div class="deletee">delete</div></ul>`]
+
+const Interior = JSON.parse(localStorage.getItem("within"))
+console.log(Interior);
+Schedule.innerHTML = Interior 
 
 
 Add.addEventListener("click", function(){
@@ -17,35 +23,49 @@ Add.addEventListener("click", function(){
    
     // Schedule.innerHTML += `<li>${InputEl.value} <label for="check"><input class="check" name="check" type="checkbox"></label></li>`
     let internal = [`<ul class="listt"><li class="listtt">${Inside}</li> <div class="deletee">delete</div></ul>`]
-   
+    // let fruits = ["apple", "orange", "banana", "watermelon", "cherry"]
+
+    
     Schedule.innerHTML += internal
+    // localStorage.setItem("within", internal)
+    localStorage.setItem("within", JSON.stringify(Schedule.innerHTML))
+    
     const Listt = document.querySelectorAll(".listt")
     const Delete = document.querySelectorAll(".deletee")
     InputEl.value = ""
     // Listt.currentTarget = Schedule.innerHTML
     console.log(Listt);
     // console.log(Inside)
-     if (Inside = ""){
-        Schedule.innerHTML = `INPUT TASK`
-    }
+    //  if (Inside = ""){
+    //     Schedule.innerHTML = `INPUT TASK`
+    // }
+    
+    // const insideData = localStorage.getItem("within")
+    // // listClick.style.display = "block"
+    // console.log(insideData);
+    
+
+
     
     Delete.forEach(function(btn){
         btn.addEventListener("click", function(e){
             let deleteClick = e.currentTarget
             console.log(deleteClick);
             
+            
             Listt.forEach(function(btnn){
-            btnn.addEventListener("click", function(c){
-                let listClick = c.currentTarget
-               console.log(listClick)
-               listClick.style.display = "none"
+                btnn.addEventListener("click", function(c){
+                    let listClick = c.currentTarget
+                    console.log(listClick)
+                    listClick.style.display = "none"
+                    
+               
             })
         
         })
-         
-        })
+        
     })
-
+})
 
 
 
@@ -53,10 +73,17 @@ Add.addEventListener("click", function(){
 
 
 })
+
+
+
+
 
 ClearBtn.addEventListener("click", function(){
     Schedule.innerHTML = ""
+    localStorage.setItem("within", JSON.stringify(internal))
+    console.log(localStorage.removeItem("within"));
 })
+
 
 
 
